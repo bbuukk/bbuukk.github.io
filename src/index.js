110abf5c44d95a777b4779e0a79a44010ee172e4
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-10-06 08:16:25 +0300

    feat: create header

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import "./utils/dom.utils.js";
+import "assets/styles/global/global.scss";
+
+//import "src/pages/main/main.page.js";
+import "src/comps/layout/header/header.js";
+
+//import Router from "./services/router.service.js";
+
+/*
+window._app = {
+  router: Router,
+  utils: {},
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elem = $("div");
+  elem?.on("click", () => console.log("Div clicked!"));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  window._app.router.init();
+});
+
+*/
