447e3637af9cbe2e5babebbca900aad5ad3a3bdb
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-12-23 16:12:33 +0200

    feat

--- todo.md
+++ todo.md
@@ -1,1 +1,3 @@
 <!-- load styles only for necessary page -->
+
+<!-- not to run home animation if user has already visited the website -->
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
 import Inspect from 'vite-plugin-inspect';
 
 import { resolve } from 'path';
@@ -8,40 +7,7 @@ export default defineConfig({
   root: 'src',
   publicDir: resolve(__dirname, './public'),
   envDir: __dirname,
-  plugins: [
-    Inspect(),
-    createHtmlPlugin({
-      minify: true,
-      /**
-       * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
-       * @default src/main.ts
-       */
-      entry: 'src/main.ts',
-      /**
-       * If you want to store `index.html` in the specified folder, you can modify it, otherwise no configuration is required
-       * @default index.html
-       */
-      // template: 'public/index.html',
-
-      /**
-       * Data that needs to be injected into the index.html ejs template
-       */
-      inject: {
-        data: {
-          injectScript: `<script src="./inject.js"></script>`
-        },
-        tags: [
-          {
-            injectTo: 'body-prepend',
-            tag: 'div',
-            attrs: {
-              id: 'tag'
-            }
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [Inspect()],
   build: {
     outDir: resolve(__dirname, './dist')
     // base: '/'
